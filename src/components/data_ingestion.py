fc9f298f48b7738ed4b2d29c576a1130e9ae44f9
bharatAmeria <the.bharat.ameria@gmail.com>
2023-10-13 12:23:13 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 from src.components.model_trainer import ModelTrainer
 from src.exception import CustomException
 from src.loggers import logging
